e5a06f84b29880c160de47a998b3a87f658355cc
ndurushima <ndu1994@gmail.com>
2025-08-15 20:28:11 -0500

    added gets, patch, delete routes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
-from models import list_items, create_item
-from utils import ok, json_required, bad_request
+from models import list_items, create_item, update_item, get_items, delete_item
+from utils import ok, json_required, bad_request, not_found
+import requests
 
 app = Flask(__name__)
 
@@ -17,8 +18,8 @@ def create_item_route():
             price=data["price"]
         )
         return ok(item, 201)
-    except ValueError:
-        return bad_request("data validation error")
+    except ValueError as e:
+        return bad_request(str(e))
 
 
 @app.get("/items")
@@ -26,6 +27,30 @@ def list_items_route():
     category = request.args.get("category")
     items = list_items(category)
     return ok({"count": len(items), "items":items})
+
+@app.get("/items/<item_id>")
+def get_item_route(item_id):
+    item = get_items(item_id)
+    if not item:
+        return not_found("Item not found")
+    return ok(item)
+
+@app.patch("/items/<item_id>")
+def update_item_route(item_id):
+    if not get_items(item_id):
+        return not_found("Item not found")
+    data = request.get_json(silent=True) or {}
+    try:
+        updated = update_item(item_id, **data)
+        return ok(updated)
+    except ValueError as e:
+        return bad_request(str(e))
+
+@app.delete("/items/<item_id>")
+def delete_item_route(item_id):
+    if delete_item(item_id):
+        return ok({"deleted": item_id})
+    return not_found("Item not found")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5555)
