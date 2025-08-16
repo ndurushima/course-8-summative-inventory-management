3726aa88b45becc7446269c8ebb0896bde36bc2f
ndurushima <ndu1994@gmail.com>
2025-08-16 08:41:56 -0500

    started the cli.py file

---
+++ cli.py
@@ -0,0 +1,14 @@
+import click, requests
+import argparse
+
+# will insert API once I know how
+
+
+def cli():
+    parser = argparse.ArgumentParser(description="Inventory Management")
+    subparsers = parser.add_subparsers()
+
+
+
+if __name__ == "__main__":
+    cli()
